2f9fa864cb079e90384f7a50862762e0184a2320
ming-sil <choming4926@gmail.com>
2022-07-29 06:08:44 +0900

    Product Page Section 3,4

--- src/components/pages/product/Product.js
+++ src/components/pages/product/Product.js
@@ -1,10 +1,23 @@
+import styled from "styled-components";
 import { PageTitle } from "../../../PageTitle";
+import { Section1 } from "./Section1";
+import { Section2 } from "./Section2";
+import { Section3 } from "./Section3";
+import { Section4 } from "./Section4";
+
+
+const Wrap = styled.div`
+  width: 100%;
+`;
 
 export const Product = () => {
   return (
-    <>
+    <Wrap>
       <PageTitle title="Product" />
-      Product
-    </>
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+    </Wrap>
   );
 };
