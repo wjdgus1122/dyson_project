ba8ec7d46c28a67cd568172c698b489ee2c72d91
yujinkim11 <wowyj26@naver.com>
2022-08-03 01:02:52 +0900

    footer responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 import { mainStyle } from "../styles/Globalstyle";
+import responsiveLogo from "./Image/responsivelogo.png";
 
 const FooterAllWrap = styled.div`
   width: 100%;
   display: flex;
   position: relative;
+  @media screen and (max-width: 500px) {
+    width: 100%;
+  }
 `;
 
 const FooterBg = styled.div`
@@ -14,6 +18,10 @@ const FooterBg = styled.div`
   backdrop-filter: blur(5px);
   position: absolute;
   bottom: 0;
+  @media screen and (max-width: 500px) {
+    width: 100%;
+    height: 300px;
+  }
 `;
 
 const FooterWrap = styled.div`
@@ -26,7 +34,6 @@ const FooterWrap = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-
   h3 {
     font-size: 20px;
     font-weight: 700;
@@ -34,6 +41,23 @@ const FooterWrap = styled.div`
   a {
     color: ${mainStyle.blackColor};
     opacity: 0.7;
+  }
+  @media screen and (max-width: 500px) {
+    width: 100%;
+    height: 300px;
+    padding: ${mainStyle.mopadding};
+    display: flex;
+    align-items: flex-start;
+  }
+`;
+
+const Logo = styled.div`
+  width: 83px;
+  height: 31px;
+  display: none;
+  @media screen and (max-width: 500px) {
+    display: block;
+    margin: 15px 0;
   }
 `;
 
@@ -46,22 +70,55 @@ const FooterLeft = styled.div`
     font-size: 20px;
     font-weight: 300;
   }
+  .mail {
+    display: none;
+  }
   @media screen and (max-width: 500px) {
-    padding-left: 50px;
+    padding-left: 0px;
+    width: 200px;
+    line-height: 18px;
+    .slash {
+      display: none;
+    }
+    .mail {
+      display: block;
+      font-size: 12px;
+      font-weight: 300;
+    }
+    h3 {
+      font-size: 10px;
+      font-weight: 700;
+      margin-bottom: 5px;
+    }
+    p {
+      font-size: 12px;
+      font-weight: 300;
+      span {
+        font-size: 15px;
+        font-weight: 700;
+      }
+    }
   }
 `;
 const FooterCenter = styled.div`
   text-align: center;
   h3 {
-    margin-bottom: 10px;
+    margin-bottom: 60px;
+    text-align: center;
+    margin-left: 60px;
   }
-  p {
-    margin-bottom: 30px;
-  }
-  span {
-    font-size: 10px;
+
+  @media screen and (max-width: 500px) {
+    h3 {
+      display: none;
+    }
+    p {
+      font-size: 10px;
+      font-weight: 300;
+    }
   }
 `;
+
 const FooterRight = styled.div`
   padding-right: 120px;
   text-align: right;
@@ -74,7 +131,76 @@ const FooterRight = styled.div`
     margin-bottom: 20px;
   }
   @media screen and (max-width: 500px) {
-    padding-right: 50px;
+    h3 {
+      display: none;
+    }
+    p {
+      width: 170px;
+      font-size: 12px;
+      font-weight: 500;
+      text-align: left;
+    }
+    a {
+      text-align: center;
+    }
+    position: absolute;
+    bottom: 60px;
+  }
+`;
+
+const Icon = styled.div`
+  :hover {
+    cursor: pointer;
+  }
+  @media screen and (max-width: 500px) {
+    margin-left: 80px;
+    span {
+      font-size: 30px;
+    }
+  }
+`;
+
+const FooterBottom = styled.div`
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const User = styled.div`
+  display: flex;
+  margin-top: -100px;
+  margin-bottom: 40px;
+  font-size: 14px;
+  font-weight: 300;
+  :hover {
+    cursor: pointer;
+  }
+  span {
+    margin: 0 5px;
+  }
+  @media screen and (max-width: 500px) {
+    span {
+      display: none;
+    }
+    display: flex;
+    flex-direction: column;
+    text-align: right;
+    position: absolute;
+    right: 20px;
+    bottom: 125px;
+    line-height: 25px;
+  }
+`;
+
+const CopyRight = styled.div`
+  font-size: 10px;
+  font-weight: 300;
+  @media screen and (max-width: 500px) {
+    margin-bottom: 20px;
   }
 `;
 
@@ -86,51 +212,71 @@ export const Footer = () => {
 
         <FooterWrap>
           <FooterLeft>
+            <Logo
+              style={{
+                background: `url(${responsiveLogo}) no-repeat center / cover`,
+              }}
+            ></Logo>
             <h3>문의 연락처</h3>
             <p>
-              <span>080-300-4253</span>(수신자 부담) / <span>1588-4253</span>
+              <span>080-300-4253</span>(수신자 부담){" "}
+              <span className="slash">/</span> <span>1588-4253</span>
               <br /> (운영시간: 월-금 오전 9시-오후 6시)
+              <span className="mail">이메일:help@kr.dyson.com</span>
             </p>
           </FooterLeft>
           <FooterCenter>
             <h3>다이슨 코리아 유한회사</h3>
-            <p>개인정보방침 | 웹사이트 사용 약관 | 사업자 정보 확인</p>
-            <span>© Jeonghyeon | Yujin | Minseo | Heejeong</span>
           </FooterCenter>
           <FooterRight>
             <h3>위치 안내</h3>
             <p>서울특별시 강남구 테헤란로 142, 아크플레이스 17층 06236</p>
+            <Icon>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.youtube.com/dyson"
+              >
+                <span>
+                  <i class="fa-brands fa-youtube"></i>
+                </span>
+              </a>
 
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.youtube.com/dyson"
-            >
-              <span>
-                <i class="fa-brands fa-youtube"></i>
-              </span>
-            </a>
-
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.instagram.com/dyson_kr/"
-            >
-              <span>
-                <i class="fa-brands fa-instagram"></i>
-              </span>
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://ko-kr.facebook.com/DysonKorea/about/"
-            >
-              <span>
-                <i class="fa-brands fa-square-facebook"></i>
-              </span>
-            </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.instagram.com/dyson_kr/"
+              >
+                <span>
+                  <i class="fa-brands fa-instagram"></i>
+                </span>
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://ko-kr.facebook.com/DysonKorea/about/"
+              >
+                <span>
+                  <i class="fa-brands fa-square-facebook"></i>
+                </span>
+              </a>
+            </Icon>
           </FooterRight>
         </FooterWrap>
+        <FooterBottom>
+          <User>
+            <p>
+              개인정보방침 <span>|</span>
+            </p>
+            <p>
+              웹사이트 사용 약관<span>|</span>
+            </p>
+            <p>사업자 정보 확인</p>
+          </User>
+          <CopyRight>
+            <span>© Jeonghyeon | Yujin | Minseo | Heejeong</span>
+          </CopyRight>
+        </FooterBottom>
       </FooterAllWrap>
     </>
   );
