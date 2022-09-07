dfea9e85549562b66497887a0c5d1e94b1a1cb72
hijeong_ <gmlwjdchl82@naver.com>
2022-09-07 22:12:00 +0900

    scroll btn

--- src/components/ScrollBtn.js
+++ src/components/ScrollBtn.js
@@ -2,39 +2,55 @@ import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { mainStyle } from "../styles/Globalstyle";
 
 export const ScrollBtn = () => {
-  const [scrBtn, setScrBtn] = useState(0);
+  const [scrBtn, setScrBtn] = useState(false);
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
+    const handleScr = () => {
       if (window.scrollY > 100) {
         setScrBtn(true);
       } else {
         setScrBtn(false);
       }
-    });
+    };
+    window.addEventListener("scroll", handleScr);
+    // return () => {
+    //   window.removeEventListener("scroll", handleScr);
+    // };
   }, []);
 
-  const scrollUp = () => {
+  const onClick = () => {
     window.scrollTo({
       top: 0,
-      behavior: "auto",
+      behavior: "smooth",
     });
   };
 
   return (
     <>
-      <Btn onClick={scrollUp}>
-        <FontAwesomeIcon icon={faChevronUp} />
-      </Btn>
+      {scrBtn && (
+        <Btn onClick={onClick}>
+          <FontAwesomeIcon icon={faChevronUp} />
+        </Btn>
+      )}
     </>
   );
 };
 
 const Btn = styled.button`
   all: unset;
+  width: 50px;
+  height: 80px;
+  background-color: rgba(255, 255, 255, 0.5);
+  text-align: center;
+  line-height: 80px;
+  border-radius: 50px;
   position: fixed;
   bottom: 50px;
   right: 50px;
+  &:hover {
+    background-color: rgba(${mainStyle.mainRgbColor}, 0.5);
+  }
 `;
